83ddcd72e7085a936fc4cfa2554bbdde04731c8e
Evan Fleet <evfleet@gmail.com>
2021-09-27 14:38:45 -0400

    feat(api): auth boilerplate

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { TweetsModule } from './tweets/tweets.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, TweetsModule, DatabaseModule],
+  imports: [UsersModule, TweetsModule, DatabaseModule, AuthModule],
+  providers: [],
 })
 export class AppModule {}
