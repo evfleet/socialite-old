6966369583968571f1631296371c966f2c85143f
Evan Fleet <evfleet@gmail.com>
2021-09-27 00:18:30 -0400

    chore(api): remove unnecessary files from boilerplate

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetsModule } from './tweets/tweets.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [UsersModule, TweetsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
