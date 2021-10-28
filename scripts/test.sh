28f4f3d1e1d0081a4cdfe61c182a84b64a29a775
Evan Fleet <evfleet@gmail.com>
2021-10-28 15:19:19 -0400

    build(root): improve testing workflow with docker and fix some issues

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-[ "$WATCH" ] &&
-  SCRIPT="npm run test:watch"  ||
-  SCRIPT="npm run test"
-
-COMMAND=$SCRIPT docker-compose -f docker-compose.yml -f docker-compose.test.yml up
-
+if [ "$WATCH" ]
+then
+  docker-compose -f docker-compose.yml -f docker-compose.test.yml up database test-watch
+else
+  docker-compose -f docker-compose.yml -f docker-compose.test.yml up database test
+fi
