9740b5fb105fd3f402a335d04b8e2a5da7e98dad
Evan Fleet <evfleet@gmail.com>
2021-10-28 02:53:33 -0400

    test(api): fix some issues with database not being imported in tests

--- packages/api/src/users/users.controller.spec.ts
+++ packages/api/src/users/users.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from '../database/database.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
@@ -7,6 +8,7 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
