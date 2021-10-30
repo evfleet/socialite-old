3c2c9f315ed2ea9cec905286b31d60d55bb395f3
Evan Fleet <evfleet@gmail.com>
2021-10-30 15:15:15 -0400

    feat(api): boilerplate for auth register

--- packages/api/src/auth/auth.service.ts
+++ packages/api/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  register(user: any) {
+    return { username: 'Test' };
+  }
+}
